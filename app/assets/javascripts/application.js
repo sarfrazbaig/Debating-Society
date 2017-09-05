36aff4aac204a257c2853043c55e4ac5f5250826
sarfrazbaig <sarfrazbaig55@gmail.com>
2017-09-05 11:20:50 +0530

    Added modal for signing in and styled it

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,8 @@
  $(document).ready(function(){
       $('.parallax').parallax();
    });
+
+   $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
