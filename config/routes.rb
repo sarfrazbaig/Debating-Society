5e584a8aa65b27af7f589c1ca84948e60d8461e2
sarfrazbaig <sarfrazbaig55@gmail.com>
2017-09-05 10:44:30 +0530

    Added a home button

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#land'
   get '/auth/:provider/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get '/index', to: 'pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
